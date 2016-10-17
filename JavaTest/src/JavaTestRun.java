5a56ecf2207d06b69f4cc13b9c533821dc23c860
Nicjack <korchl@nate.com>
2016-10-17 17:46:33 +0900

    # WARNING: head commit changed in the meantime
    first commit

---
+++ JavaTest/src/JavaTestRun.java
@@ -0,0 +1,14 @@
+
+public class JavaTestRun {
+
+	public JavaTestRun() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Run!!!");
+		System.out.println("Test Run!!!");
+	}
+
+}
